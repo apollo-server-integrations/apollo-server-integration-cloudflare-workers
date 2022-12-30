1e8f05c5dfba51cddd77efbc42a573fcc6a3f543
Nguyen Huu Kim <kimnh@webee.asia>
2022-12-30 10:05:23 +0700

    feat: initialize apollo server integration for Cloudflare Workers

---
+++ README.md
@@ -0,0 +1,72 @@
+# apollo-server-integration-cloudflare-workers
+
+This is an Apollo Server v4 integration for Cloudflare Workers.
+
+## Demo
+
+- Source: https://github.com/kimyvgy/worker-apollo-server-template
+- Live demo: https://worker-apollo-server.webee-asia.workers.dev
+
+## Install
+
+```bash
+npm install @apollo/server graphql @as-integrations/cloudflare-workers
+```
+
+## Usage
+
+```typescript
+import type { CloudflareWorkersHandler } from 'apollo-server-integration-cloudflare-workers';
+
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { startServerAndCreateCloudflareWorkersHandler } from 'apollo-server-integration-cloudflare-workers';
+
+interface ApolloDataSources {
+  pokemonAPI: PokemonAPI;
+}
+
+interface ContextValue {
+  token: string;
+  dataSources: ApolloDataSources;
+};
+
+const server = new ApolloServer<ContextValue>({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    // ApolloServerPluginLandingPageProductionDefault({
+    //   graphRef: 'my-graph-id@my-graph-variant',
+    //   footer: false,
+    // })
+  ],
+});
+
+const handleGraphQLRequest: CloudflareWorkersHandler = startServerAndCreateCloudflareHandler(server, {
+  context: async ({ request }) => {
+    const token = request.headers.token || '';
+    const cache = server.cache;
+
+    const dataSources: ApolloDataSources = {
+      pokemonAPI: new PokemonAPI({ cache, token }),
+    };
+
+    return { dataSources, token };
+  },
+});
+
+// 1. ServiceWorker syntax:
+// --------------------------------------------------------------------
+addEventListener((e) => handleGraphQLRequest(e.request as Request));
+// --------------------------------------------------------------------
+
+// 2. Or ModuleWorker syntax:
+// --------------------------------------------------------------------
+// export default {
+//   fetch(request) {
+//     return handleGraphQLRequest(request);
+//   },
+// };
+```
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import type { WithRequired } from '@apollo/utils.withrequired';
+
+import {
+  ApolloServer,
+  BaseContext,
+  ContextFunction,
+  HeaderMap,
+  HTTPGraphQLRequest,
+} from '@apollo/server';
+
+export type CloudflareWorkersHandler = (request: Request) => Promise<Response>;
+
+export interface CloudflareContextFunctionArgument {
+  request: Request;
+}
+
+export interface CloudflareWorkersHandlerOptions<TContext extends BaseContext> {
+  context?: ContextFunction<[CloudflareContextFunctionArgument], TContext>;
+}
+
+export function startServerAndCreateCloudflareWorkersHandler(
+  server: ApolloServer<BaseContext>,
+  options?: CloudflareWorkersHandlerOptions<BaseContext>,
+): CloudflareWorkersHandler;
+export function startServerAndCreateCloudflareWorkersHandler<TContext extends BaseContext>(
+  server: ApolloServer<TContext>,
+  options: WithRequired<CloudflareWorkersHandlerOptions<TContext>, 'context'>,
+): CloudflareWorkersHandler;
+export function startServerAndCreateCloudflareWorkersHandler<TContext extends BaseContext>(
+  server: ApolloServer<TContext>,
+  options?: CloudflareWorkersHandlerOptions<TContext>,
+): CloudflareWorkersHandler {
+  server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
+
+  const defaultContext: ContextFunction<
+    [CloudflareContextFunctionArgument],
+    any
+  > = async () => ({});
+
+  const contextFunction: ContextFunction<
+    [CloudflareContextFunctionArgument],
+    TContext
+  > = options?.context ?? defaultContext;
+
+  return async (request: Request) => {
+    try {
+      if (request.method === 'OPTIONS') {
+        return new Response('', { status: 204 });
+      }
+
+      const httpGraphQLRequest = await normalizeIncomingRequest(request);
+
+      const { body, headers, status } = await server.executeHTTPGraphQLRequest({
+        httpGraphQLRequest: httpGraphQLRequest,
+        context: () => contextFunction({ request }),
+      });
+
+      if (body.kind === 'chunked') {
+        throw Error('Incremental delivery not implemented');
+      }
+
+      return new Response(body.string, {
+        status: status || 200,
+        headers: {
+          ...Object.fromEntries(headers),
+          'content-length': Buffer.byteLength(body.string).toString(),
+        },
+      });
+    } catch (e) {
+      return new Response((e as Error).message, { status: 400 });
+    }
+  }
+}
+
+async function normalizeIncomingRequest(request: Request): Promise<HTTPGraphQLRequest> {
+  const headers = normalizeHeaders(request.headers);
+  const url = new URL(request.url);
+  const method = request.method.toUpperCase();
+
+  return {
+    method,
+    headers,
+    body: method === 'GET' ? request.body : await request.json(),
+    search: url.search ?? '',
+  }
+}
+
+function normalizeHeaders(headers: Headers): HeaderMap {
+  const headerMap = new HeaderMap();
+
+  headers.forEach((value, key) => {
+    headerMap.set(key, Array.isArray(value) ? value.join(', ') : value);
+  });
+
+  return headerMap;
+}
